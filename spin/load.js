4bcf5543b7a3d73385ba142235606dceef6e0209
d-l0g <96639173+d-l0g@users.noreply.github.com>
2022-03-11 01:25:25 +0500

    Create load.js

---
+++ spin/load.js
@@ -0,0 +1,6 @@
+ var a = document.getElementById("loader");
+document.getElementsByTagName("body")[0], setTimeout((function() {
+a.style.opacity = 0
+}), 5000), setTimeout((function() {
+a.style.display = "none"
+}), 1e3)
